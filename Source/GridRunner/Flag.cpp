1cf21fddbb7ea417a65ca8c052beaac50c97a78c
Game.Play() <gamedotplay0@gmail.com>
2022-04-11 11:24:03 -0400

    Update GridRunner
    Added Flag Actor class.
    Added blue material for male character.

---
+++ Source/GridRunner/Flag.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Flag.h"
+#include "Components/SphereComponent.h"
+#include "GridRunnerGameMode.h"
+
+// Sets default values
+AFlag::AFlag()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	this->SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	this->SetRootComponent(this->SphereComponent);
+
+	this->FlagMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FlagMesh"));
+}
+
+// Called when the game starts or when spawned
+void AFlag::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFlag::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
