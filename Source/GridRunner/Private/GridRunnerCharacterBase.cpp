dfb02d0614c069d9453b8409fe662aca362bd940
Game.Play() <gamedotplay0@gmail.com>
2022-04-21 18:47:04 -0400

    Update GridRunnerCharacterBase.cpp
    Modified Tick() logic for hiding IT X.
    Removed OnComponentOverlap into Player.

--- Source/GridRunner/Private/GridRunnerCharacterBase.cpp
+++ Source/GridRunner/Private/GridRunnerCharacterBase.cpp
@@ -7,52 +7,29 @@
 AGridRunnerCharacterBase::AGridRunnerCharacterBase()
 {
  	this->CharacterIsIt = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CharacterIsIt"));
+	this->GetCapsuleComponent()->SetGenerateOverlapEvents(true);
 }
 
 // Called when the game starts or when spawned
 void AGridRunnerCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-
-	this->GetCapsuleComponent()->OnComponentBeginOverlap.AddDynamic(this, &AGridRunnerCharacterBase::OtherCharacterTouched);
 }
 
 void AGridRunnerCharacterBase::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
 
+	FVector MyLocation = GetActorLocation();
+	this->CharacterIsIt->SetWorldLocation(FVector(MyLocation.X, MyLocation.Y, MyLocation.Z + 100.f));
+	this->CharacterIsIt->AddWorldRotation(FRotator(0, this->XRotateSpeed * DeltaSeconds, 0));
+
 	if (this->bIsIt)
 	{
-		this->CharacterIsIt->bHiddenInGame = false;
-		this->CharacterIsIt->SetWorldLocation(FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z + 100.f));
-		this->CharacterIsIt->AddWorldRotation(FRotator(0, this->XRotateSpeed * DeltaSeconds, 0));
+		this->CharacterIsIt->SetHiddenInGame(false);
 	}
 	else
 	{
-		this->CharacterIsIt->bHiddenInGame = true;
-	}
-}
-
-void AGridRunnerCharacterBase::OtherCharacterTouched(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	if (!ensure(OtherActor))
-	{
-		return; // OtherActor is null.
-	}
-
-	if (!Cast<AGridRunnerCharacterBase>(OtherActor))
-	{
-		return; // OtherActor is not the player or the opponent. Do nothing...for now.
-	}
-
-	if (Cast<AGridRunnerCharacterBase>(OtherActor)->bIsIt) // We got tagged.
-	{
-		Cast<AGridRunnerCharacterBase>(OtherActor)->bIsIt = false;
-		this->bIsIt = true;
-	}
-	else
-	{
-		Cast<AGridRunnerCharacterBase>(OtherActor)->bIsIt = true;
-		this->bIsIt = false;
+		this->CharacterIsIt->SetHiddenInGame(true);
 	}
 }
