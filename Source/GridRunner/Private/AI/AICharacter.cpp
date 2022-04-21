bf0042f5510f4d948837c8c2e197e2a172a56923
Game.Play() <gamedotplay0@gmail.com>
2022-04-21 15:54:49 -0400

    Added a call to the GameMode to register this class as the opponent.

--- Source/GridRunner/Private/AI/AICharacter.cpp
+++ Source/GridRunner/Private/AI/AICharacter.cpp
@@ -2,10 +2,19 @@
 
 #include "AI/AICharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "GridRunner/GridRunnerGameMode.h"
 
 AAICharacter::AAICharacter()
 {
     
+}
+
+void AAICharacter::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+    
+    Cast<AGridRunnerGameMode>(UGameplayStatics::GetGameMode(this))->CacheOpponentCharacter(this);
 }
 
 void AAICharacter::BeginPlay()
@@ -13,6 +22,7 @@ void AAICharacter::BeginPlay()
     Super::BeginPlay();
 
     GetCharacterMovement()->MaxWalkSpeed = this->RunSpeed;
+    this->bIsPlayer = false;
 }
 
 void AAICharacter::Tick(float DeltaSeconds)
